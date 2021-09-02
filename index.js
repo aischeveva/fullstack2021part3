eee544d1a28ffe8f78f3b92b5f6afc3724143314
Alena Shchevyeva <alena.shchevyeva@aalto.fi>
2021-09-02 22:15:35 +0300

    added procfile

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { response, request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('data', function(req){if(req.method==='POST') {return JSON.stringify(req.body)} else {return ' '}})
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
@@ -81,7 +83,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
