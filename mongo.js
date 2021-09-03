9ee839cfd7c7fe72d9058162e12d69937e691d5c
Alena Shchevyeva <alena.shchevyeva@aalto.fi>
2021-09-03 19:36:47 +0300

    3.12 done

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+  }
+  
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.ecchv.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phone: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.phone}`)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 5){
+    const person = new Person({
+        name: process.argv[3],
+        phone: process.argv[4]
+      })
+      
+      person.save().then(result => {
+        console.log(`added ${result.name} number ${result.phone} to the phonebook`)
+        mongoose.connection.close()
+      })
+} else {
+    console.log('Input has to be of form: node mongo.js <password> <name> <phone number>')
+    process.exit(1)
+}
